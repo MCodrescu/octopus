b8fbb7099ce45e234e191f9f7da85550703028d5
Codrescu, Marcus (mc678p) <mc678p@att.com>
2023-03-21 16:00:16 -0500

    Add a skeleton test file for the shiny app itself.

---
+++ tests/testthat/test-view_database.R
@@ -0,0 +1,18 @@
+## Checklist of Manual Tests
+# Check that schemas are shown
+# Check that tables are shown
+# Check that changes a schema changes the tables
+# Check that clicking View button shows a modal with the table
+# Check that downloading the table works
+# Check that creating a new table works
+# Check that removing that new table works
+# Check that a simple SELECT * FROM mtcars query works
+# Check that formatting a query works
+# Check that the collapsing looks good on every screen size.
+
+test_that(
+  "the checklist was completed",
+  {
+    expect_true(TRUE)
+  }
+)
